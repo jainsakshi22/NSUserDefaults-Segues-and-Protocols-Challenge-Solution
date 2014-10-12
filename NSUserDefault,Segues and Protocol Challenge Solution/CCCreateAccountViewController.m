71492aafeb3646e2acb95d02266a10af79a02951
Sakshi Jain <sakshi@Sakshis-MacBook-Pro.local>
2014-10-12 21:46:47 +0530

    Persist create account information to NSUserDefaults

--- NSUserDefault,Segues and Protocol Challenge Solution/CCCreateAccountViewController.m
+++ NSUserDefault,Segues and Protocol Challenge Solution/CCCreateAccountViewController.m
@@ -36,7 +36,20 @@
 
 - (IBAction)createAccountButtonPressed:(UIButton *)sender
 {
-    [self.delegate didCreateAccount];
+    if ((self.usernameTextFields.text.length !=0) && (self.passwordTextField.text.length !=0) && [self.passwordTextField.text isEqualToString:self.confirmPasswordTextField.text] )
+    {
+        [[NSUserDefaults standardUserDefaults] setObject:self.usernameTextFields.text forKey:USER_NAME];
+        [[NSUserDefaults standardUserDefaults] setObject:self.passwordTextField.text forKey:USER_PASSWORD];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        [self.delegate didCreateAccount];
+     }
+    else
+    {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Information not entered correctly" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+        
+        [alertView show];
+    }
 }
 
 - (IBAction)cancelButtonPressed:(UIButton *)sender
